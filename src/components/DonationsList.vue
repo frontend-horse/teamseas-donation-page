1cef5fcbe039468cdcd345d1bbe0ec8ed5bf721d
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-20 22:59:54 -0500

    Add sponsors and total donated to site

--- src/components/DonationsList.vue
+++ src/components/DonationsList.vue
@@ -19,7 +19,10 @@ const isLoading = ref(true);
 const errorOccurred = ref(false);
 
 const topDonors = ref<TopDonor[] | null>(null);
-const latestDonations = ref<Donation[] | null>(null);
+const allDonations = ref<Donation[] | null>(null);
+let totalDonationAmount = ref<number>(0);
+const sponsorDonationIDs = [8748308, 8749386, 8751610];
+let sponsorDonations = ref<Donation[] | null>(null);
 
 onMounted(async () => {
   try {
@@ -29,12 +32,16 @@ onMounted(async () => {
       throw error;
     }
 
-    latestDonations.value =
+    allDonations.value =
       data?.sort((a, b) => b.created_at - a.created_at) || null;
+
+    const reducer = (previousValue, currentValue) =>
+      previousValue + currentValue.donation;
+    totalDonationAmount.value = data.reduce(reducer, 0);
 
     topDonors.value =
       data
-        ?.reduce((donorsArr: TopDonor[], { display_name, donation }) => {
+        ?.reduce((donorsArr: TopDonor[], { display_name, donation, id }) => {
           const currentNameEntryIndex = donorsArr.findIndex(
             (donor) => donor.display_name === display_name
           );
@@ -42,13 +49,23 @@ onMounted(async () => {
           if (currentNameEntryIndex !== -1) {
             donorsArr[currentNameEntryIndex].donation += donation;
           } else {
-            donorsArr.push({ display_name, donation });
+            donorsArr.push({ display_name, donation, id });
           }
 
           return donorsArr;
         }, [])
+        .filter((item) => {
+          return !sponsorDonationIDs.includes(item.id);
+        })
         .sort((a, b) => b.donation - a.donation)
         .slice(0, 10) || null;
+    sponsorDonations.value = sponsorDonationIDs.map((id) =>
+      data.find((item) => item.id === id)
+    );
+    console.log(
+      '🚀 ~ file: DonationsList.vue ~ line 65 ~ onMounted ~ sponsorDonations',
+      sponsorDonations
+    );
   } catch (error) {
     errorOccurred.value = true;
   } finally {
@@ -69,21 +86,37 @@ onMounted(async () => {
         <TopDonors :donors="topDonors" />
       </div>
     </section>
-    <article class="all-donations">
+    <section class="">
+      <div>
+        <h2>Sponsor Donations</h2>
+        <TopDonors :donors="sponsorDonations" />
+      </div>
+    </section>
+    <section>
+      <h2>Total Amount Donated</h2>
+      <p class="donation-total">${{ totalDonationAmount }}</p>
+    </section>
+    <section class="all-donations">
       <h2 class="heading">All Donations</h2>
       <div class="grid">
         <DonationCard
-          v-for="donation in latestDonations"
+          v-for="donation in allDonations"
           :key="donation.id"
           :donation="donation"
         />
       </div>
-    </article>
+    </section>
   </div>
 </template>
 
 <style scoped>
 #error-message {
+  text-align: center;
+}
+
+.donation-total {
+  font-size: 3rem;
+  font-weight: bold;
   text-align: center;
 }
 </style>
